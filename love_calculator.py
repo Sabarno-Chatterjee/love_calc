029824bab0a07687a3580be8465548fe9aa5d8a6
Sabarno Chatterjee <106575210+Sabarno-Chatterjee@users.noreply.github.com>
2022-09-13 20:43:33 +0530

    Add files via upload

--- love_calculator.py
+++ love_calculator.py
@@ -6,35 +6,31 @@ name2 = input("What is their name? \n")
 
 #Write your code below this line 👇
 
-n1 = name1.lower()
-n2 = name2.lower()
+combo = name1 + name2
+combo = combo.lower()
 
+t = combo.count("t")
+r = combo.count("r")
+u = combo.count("u")
+e = combo.count("e")
 
+true = t + r + u + e
 
-t= n1.count("t") + n2.count("t")
-r= n1.count("r") + n2.count("r")
-u= n1.count("u") + n2.count("u")
-e= n1.count("e") + n2.count("e")
+l = combo.count("l")
+o = combo.count("o")
+v = combo.count("v")
+e = combo.count("e")
 
-true= t+r+u+e
+love = l + o + v + e
 
-l= n1.count("l") + n2.count("l")
-o= n1.count("o") + n2.count("o")
-v= n1.count("v") + n2.count("v")
-e= n1.count("e") + n2.count("e")
+love_score = str(true) + str(love)
+love_score = int(love_score)
 
-
-love= l+o+v+e
-
-love_score= str(true)+str(love)
-love_score= int(love_score)
-
-if love_score <= 10 or love_score >= 90:
+if love_score < 10 or love_score > 90:
     print(f"Your score is {love_score}, you go together like coke and mentos.")
 elif love_score >= 40 and love_score <= 50:
     print(f"Your score is {love_score}, you are alright together.")
 else:
     print(f"Your score is {love_score}.")
-
 
 
